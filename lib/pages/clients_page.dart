f24b4ec67df8d37159db90274a54153eb4ac47dd
Sandro Ferreira <sandrorf1973@gmail.com>
2023-10-24 09:19:36 -0300

    Segundo Commit

--- lib/pages/clients_page.dart
+++ lib/pages/clients_page.dart
@@ -82,7 +82,6 @@ class _ClientsPageState extends State<ClientsPage> {
                 );
               }
               return Container();
-              //Comentarioa
             }),
       ),
     );
